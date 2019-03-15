abbc8db6ae979552f82edb57880725cf0c1fb1bc
Nania <nania.anabela22@gmail.com>
2019-03-15 15:11:19 +0700

    merevisi kata2 car rent menjadi rent car

--- CarRent/application/views/element/mainuser.php
+++ CarRent/application/views/element/mainuser.php
@@ -34,7 +34,7 @@
 
             <div class="col-12 mb-3 mb-md-0 col-md-4 order-1 order-md-2 text-center">
               <div class="site-logo">
-                <a href="index.html" class="js-logo-clone">Car Rent "Barokah"</a>
+                <a href="index.html" class="js-logo-clone">Rent Car "Barokah"</a>
               </div>
             </div>
 
