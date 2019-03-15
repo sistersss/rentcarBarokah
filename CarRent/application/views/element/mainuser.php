ce7ec9472275cda824a42bf89b0b07903d9234f6
Nania <nania.anabela22@gmail.com>
2019-03-15 14:12:52 +0700

    testing commit

--- CarRent/application/views/element/mainuser.php
+++ CarRent/application/views/element/mainuser.php
@@ -162,7 +162,7 @@
       <div class="container">
         <div class="row justify-content-center">
           <div class="col-md-7 site-section-heading text-center pt-4">
-            <h2>Mobil Terlaris</h2>
+            <h2>BANYAK DIMINATI</h2>
           </div>
         </div>
         <div class="row">
