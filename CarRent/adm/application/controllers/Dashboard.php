9cd80e2e1212c122e541cfc94de6e132c3e4e8cc
TikaYulian27 <tikayulian27@gmail.com>
2019-05-25 00:56:38 +0700

    test commit

--- CarRent/adm/application/controllers/Dashboard.php
+++ CarRent/adm/application/controllers/Dashboard.php
@@ -3,21 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+
 	public function index()
 	{
 		$data['title'] = 'Dashboard';
@@ -44,7 +30,7 @@ class Dashboard extends CI_Controller {
     							'nama_admin'=>$show->nama_admin);
     			$this->session->set_userdata($dlogin);
 				redirect(base_url().'Dashboard');
-				
+
 				//echo "<script>alert('Berhasil Login');</script>";
     		}
     		else
